dacedbb59c941b89b3183935b23aad9c8ddaed3a
Josh Jarabek <josh@parakeet.ventures>
2025-02-13 00:50:33 -0600

    Initial commit

---
+++ solstice/solsticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  solsticeApp.swift
+//  solstice
+//
+//  Created by Woof on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct solsticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
